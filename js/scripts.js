c57823a2e7b681eadf9bbc1dab2451ee46f13293
Shokouh Farvid and Sid Benavente <shokouh+sid@>
2016-06-22 10:41:06 -0700

    add and test user interface

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("form#leap-year").submit(function(){
+    event.preventDefault();
+    var year = parseInt($("#year").val());
+    // var result = leapYear(year);
+    // $("#result").text(result);
+    // TODO
+    console.log(year);
+
+  });
+});
