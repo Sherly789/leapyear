7d4452a39e5a4036f0210d00e1b4dea55d8d1980
Shokouh Farvid and Sid Benavente <shokouh+sid@>
2016-06-22 11:13:35 -0700

    test false for non leap year and true for divisible by 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
+// Global variables
+
+
+// Business Logic
+var leapYear = function(year) {
+  if (year%4===0){
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+//
+
+
+// User Logic
 $(document).ready(function(){
   $("form#leap-year").submit(function(){
     event.preventDefault();
     var year = parseInt($("#year").val());
-    // var result = leapYear(year);
-    // $("#result").text(result);
-    // TODO
-    console.log(year);
+    var result = leapYear(year);
+
+    $("#result").text(result);
 
   });
 });
